98dc606a57d97b3ea1a68ae6445e90f551a14fc7
zhangxinyue <zhangxinyue@uotc.com>
2018-08-01 18:05:10 +0800

    optimize

--- add_two_numbers/add_two_numbers.go
+++ add_two_numbers/add_two_numbers.go
@@ -5,24 +5,33 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// todo: to be implemented
+// todo: to be optimized
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	cin := 0
 	var headNode = &ListNode{Val: 0, Next: nil,}
 	currNode := headNode
-	for l1 != nil || l2 != nil {
-		var result int
-		if l1 == nil {
-			result = l2.Val + cin
-			l2 = l2.Next
-		} else if l2 == nil {
-			result = l1.Val + cin
-			l1 = l1.Next
-		} else {
-			result = l1.Val + l2.Val + cin
-			l1 = l1.Next
-			l2 = l2.Next
-		}
+	for l1 != nil && l2 != nil {
+		result := l1.Val + l2.Val + cin
+		l1 = l1.Next
+		l2 = l2.Next
+		newNode := &ListNode{Val: result % 10, Next: nil}
+		currNode.Next = newNode
+		currNode = newNode
+		cin = result / 10
+	}
+
+	for l1 != nil {
+		result := l1.Val + cin
+		l1 = l1.Next
+		newNode := &ListNode{Val: result % 10, Next: nil}
+		currNode.Next = newNode
+		currNode = newNode
+		cin = result / 10
+
+	}
+	for l2 != nil {
+		result := l2.Val + cin
+		l2 = l2.Next
 		newNode := &ListNode{Val: result % 10, Next: nil}
 		currNode.Next = newNode
 		currNode = newNode
