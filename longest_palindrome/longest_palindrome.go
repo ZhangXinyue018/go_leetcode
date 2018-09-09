ca7d6399675ff62687565114dafe1e0df521e5dc
zhangxinyue <zhangxinyue@uotc.com>
2018-09-09 12:07:18 +0800

    modify

---
+++ longest_palindrome/longest_palindrome.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func longestPalindrome(s string) string {
+	strLen := len(s)
+	longestLength := 0
+	longestStr := ""
+	for i := 0; i < strLen; i++ {
+		currLength := 0
+		j, k := i, i
+		for k < strLen-1 {
+			if s[k+1] != s[i] {
+				break
+			}
+			k++
+		}
+
+		for j > 0 && k < strLen-1 {
+			if s[j-1] != s[k+1] {
+				break
+			}
+			j--
+			k++
+		}
+		currLength = k - j + 1
+		if currLength > longestLength {
+			longestLength = currLength
+			longestStr = s[j : k+1]
+		}
+	}
+	return longestStr
+}
+
+func main() {
+	fmt.Println(longestPalindrome(""))
+	fmt.Println(longestPalindrome("babad"))
+	fmt.Println(longestPalindrome("cbbd"))
+}
