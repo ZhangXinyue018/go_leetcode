049356a5350088e62c6be3fcd13022e1c47bfe19
zhangxinyue <zhangxinyue@uotc.com>
2019-03-31 12:37:45 +0800

    change stack to common interface

--- basic/commons/commons.go
+++ basic/commons/commons.go
@@ -1,13 +1,31 @@
 package commons
 
-func CheckArrayEqual(array1 []int, array2 []int) bool {
-	if len(array1) != len(array2) {
+import "reflect"
+
+func CheckArrayEqual(array1 interface{}, array2 interface{}) bool {
+	slide1 := ReflectSlide(array1)
+	slide2 := ReflectSlide(array2)
+	if len(slide1) != len(slide2) {
 		return false
 	}
-	for i := 0; i < len(array1); i++ {
-		if array1[i] != array2[i] {
+	for i := 0; i < len(slide1); i++ {
+		if slide1[i] != slide2[i] {
 			return false
 		}
 	}
 	return true
+}
+
+func ReflectSlide(slide interface{}) []interface{} {
+	s := reflect.ValueOf(slide)
+	if s.Kind() != reflect.Slice {
+		panic("array")
+	}
+
+	ret := make([]interface{}, s.Len())
+
+	for i := 0; i < s.Len(); i++ {
+		ret[i] = s.Index(i).Interface()
+	}
+	return ret
 }
--- basic/datastructure/stack/stack.go
+++ basic/datastructure/stack/stack.go
@@ -1,19 +1,19 @@
 package stack
 
 type Stack struct {
-	Items []int
+	Items []interface{}
 }
 
-func (stack *Stack) Push(num int) {
+func (stack *Stack) Push(num interface{}) {
 	stack.Items = append(stack.Items, num)
 }
 
-func (stack *Stack) Pop() int {
+func (stack *Stack) Pop() interface{} {
 	temp := stack.Items[len(stack.Items)-1]
 	stack.Items = stack.Items[:len(stack.Items)-1]
 	return temp
 }
 
-func (stack *Stack) Top() int {
+func (stack *Stack) Top() interface{} {
 	return stack.Items[len(stack.Items)]
 }
