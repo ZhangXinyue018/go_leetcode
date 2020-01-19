88a8c3e64531b7ff279fee9e9eb9be3f9a3eb18a
zhangxinyue <Xinyue.Zhang@ibm.com>
2020-01-19 18:42:04 +0800

    add 042

--- hard/no_042/trapping_rain_water/trapping.go
+++ hard/no_042/trapping_rain_water/trapping.go
@@ -1,1 +1,37 @@
 package trapping_rain_water
+
+func trap(height []int) int {
+	return trapDetail(height, 0, len(height)-1)
+}
+
+func trapDetail(height []int, from, to int) int {
+	if from >= to-1 {
+		return 0
+	}
+	areaAcc := 0
+	heightLine, maxLine := minAndMax(height[from], height[to])
+	maxValue := 0
+	maxIndex := -1
+	for i := from + 1; i < to; i++ {
+		if height[i] > maxLine {
+			return trapDetail(height, from, i) + trapDetail(height, i, to)
+		}
+		if height[i] > maxValue {
+			maxValue = height[i]
+			maxIndex = i
+		}
+		areaAcc = areaAcc + height[i]
+	}
+	if maxValue > heightLine {
+		return trapDetail(height, from, maxIndex) + trapDetail(height, maxIndex, to)
+	}
+	return (to-from-1)*heightLine - areaAcc
+}
+
+func minAndMax(x, y int) (int, int) {
+	if x > y {
+		return y, x
+	} else {
+		return x, y
+	}
+}
